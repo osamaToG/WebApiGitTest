0b17d664d682f5edf2f65ef2dcf8f0168bc771da
Saufik <osamakhan312354@gmail.com>
2023-08-22 07:51:59 -0700

    test file

---
+++ WebApiGitTest/Controllers/GitTest.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApiGitTest.Controllers
+{
+    public class GitTest
+    {
+        public int MyProperty { get; set; }
+        public int MyProperty2 { get; set; }
+        public int MyProperty3 { get; set; }
+        public int MyProperty4 { get; set;}
+    }
+}
